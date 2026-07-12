161a68c3b3b86a528c66dcf0084fc78ec25c6a44
harisoncleytondev <harisonc081@gmail.com>
2026-07-12 09:45:20 -0300

    feat: adiciona fuso horário para o cron

--- scripts/schedule_activate.py
+++ scripts/schedule_activate.py
@@ -3,8 +3,9 @@ from scripts.drive_upload import upload
 from scripts.docker import list_containers
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
+import pytz
 
-scheduler = BlockingScheduler()
+scheduler = BlockingScheduler(timezone=pytz.timezone("America/Recife"))
 
 folder = "temp"
 
@@ -37,8 +38,9 @@ def backup():
                 print(f"Erro no upload: {e}")
 
    _del_files()
+   print("Agendador iniciado. Próximo backup às 03:00 AM (America/Recife).")
 
 def start():
-    scheduler.add_job(backup, "cron", hour=3, minute=0)
-    print("Agendador iniciado.")
+    scheduler.add_job(backup, "cron", hour=3, minute=0, timezone=pytz.timezone("America/Recife"), id="daily_backup", replace_existing=True,)
+    print("Agendador iniciado. Próximo backup às 03:00 AM (America/Recife).")
     scheduler.start()
